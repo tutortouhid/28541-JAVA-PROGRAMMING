69677d2ca06e65b91ab5ba974219739bb7e844f9
aboutSakkib <sakibhasan94470@gmail.com>
2023-06-05 12:27:23 +0600

    initial commit

---
+++ অধ্যায় --৮--ইন্টারফেস অ্যান্ড পলিমরফিজম/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+       int a =10;
+       int b=30;
+        System.out.println("before swapping");
+        System.out.println("number One ="+a);
+        System.out.println("Number two ="+b);
+
+    int store=a;  jU
+    a=b;
+    b=store;
+    System.out.println("After Swapping");
+    System.out.println("number one  = "+a);
+    System.out.println("number two  = "+b);
+
+
+
+    }
+}

82e6d11447b41361e7494ad90b80b03cdbf4bcbc
= <sakibhasan94470@gmail.com>
2023-06-05 12:47:48 +0600

    changed

--- অধ্যায় --১--জাভা  প্রোগ্রামিং এর ধারণা/Main.java
+++ অধ্যায় --১--জাভা  প্রোগ্রামিং এর ধারণা/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       int a =10;
+       int a =20;
        int b=30;
-        System.out.println("before swapping");
+        System.out.println("before   swapping");
         System.out.println("number One ="+a);
         System.out.println("Number two ="+b);
 
